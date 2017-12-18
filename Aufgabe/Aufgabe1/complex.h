15e6762953eb34019628669c6bf90d0d11326274
Wen Yi <33878886+wenyi1994@users.noreply.github.com>
2017-12-18 14:27:46 +0100

    Add files via upload

---
+++ Aufgabe/Aufgabe1/complex.h
@@ -0,0 +1,50 @@
+/*
+ *
+ * complex.h
+ * 
+ */
+
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+class MyComplex{
+
+private:
+    double m_Real;
+    double m_Image;
+
+
+public:
+    // con-/destructor
+    MyComplex();
+    MyComplex(const double re, const double im );
+    MyComplex(const MyComplex & cplx);
+    ~MyComplex();
+
+    // copy operator = 
+    MyComplex & operator=(const MyComplex & cplx);
+
+    // overload operator +-*/ (with complex number, real number on the left and on the right)
+    const MyComplex operator+(const MyComplex & cplx) const;
+    const MyComplex operator+(const double & real_number) const;
+    friend const MyComplex operator+(const double & real_number, const MyComplex & cplx); // use friend to define out of class
+    const MyComplex operator-(const MyComplex & cplx) const;
+    const MyComplex operator-(const double & real_number) const;
+    friend const MyComplex operator-(const double & real_number, const MyComplex & cplx);
+    const MyComplex operator*(const MyComplex & cplx) const;
+    const MyComplex operator*(const double & real_number) const;
+    friend const MyComplex operator*(const double & real_number, const MyComplex & cplx);
+    const MyComplex operator/(const MyComplex & cplx) const;
+    const MyComplex operator/(const double & real_number) const;
+    friend const MyComplex operator/(const double & real_number, const MyComplex & cplx);
+
+    // overload operator ^
+    const MyComplex operator^(const int & exp) const;
+
+    const double real() const;
+    const double imag() const;
+    const double norm() const;
+
+};
+
+#endif /* COMPLEX_H */ 
